dfc80f9d27e3147362e1ef6332ddc522d16fbe43
dmsqja <bum17822@naver.com>
2026-04-29 16:30:20 +0900

    MainLayout 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import NotFound from "./pages/NotFound";
 import "./App.css";
 import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
     <>
       {/* TODO: 중첩라우팅을 고려해서 총 5개 페이지에 대한 라우팅을 진행해보세요 */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/:id" element={<ProjectDetail />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </>
   );
