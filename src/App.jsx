58ef1f0edbd164ad30510cea02adf04df70c36b5
dmsqja <bum17822@naver.com>
2026-04-29 16:38:27 +0900

    Route 수정

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
       {/* TODO: 중첩라우팅을 고려해서 총 5개 페이지에 대한 라우팅을 진행해보세요 */}
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:id" element={<ProjectDetail />} />
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="projects/:id" element={<ProjectDetail />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
